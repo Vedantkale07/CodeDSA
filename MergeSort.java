b4ac06191e9043fdb9f862d299ff9e02655dd1e5
vedant Kale <vedantkale0707@gmail.com>
2024-09-18 23:23:00 +0530

    Add files via upload
    added merge sort sorting source code

---
+++ MergeSort.java
@@ -0,0 +1,59 @@
+package com.sortingAlgorithm;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args)
+    {
+        int [] arr={6,5,2,4,8,1};
+        int [] ans=mergeSort(arr);
+
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int [] mergeSort(int [] arr){
+
+        if(arr.length==1){
+            return arr;
+        }
+        int mid= arr.length/2;
+
+        int [] left= mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int [] right= mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return merge(left,right);
+    }
+
+    private static int [] merge(int []first, int [] second){
+
+        int [] mix= new int [first.length + second.length];
+
+        int i=0;
+        int j=0;
+        int k=0;
+
+        while(i<first.length && j<second.length){
+            if(first[i] < second[j]){
+                  mix[k]=first[i];
+                  i++;
+            }else{
+                mix[k]=second[j];
+                j++;
+            }
+            k++;
+        }
+        // it is possible that one array is not finish
+        // copy the remaing elements
+        while (i<first.length){
+            mix[k]=first[i];
+            i++;
+            k++;
+        }
+        while(j<second.length){
+            mix[k]=second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
