cf03882070b48e9829d2181a844f6ba4976e3c13
vedant Kale <vedantkale0707@gmail.com>
2025-07-09 20:45:43 +0530

    leetcode program is added

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -5,40 +5,39 @@ package com.leedcodeQuestions;
 public class FirstMissingPositive {
 
     public static void main(String[] args) {
-        int [] nums={1,2,0};
+        int[] nums = {1,0,3};
 
         System.out.println(firstMissingPositive(nums));
     }
-        public static int firstMissingPositive(int[] nums) {
 
-            int i=0;
-            while(i<nums.length){
-                int correct=nums[i]-1;
-                if( nums[i] >0 && nums[i]<= nums.length &&  nums[i] != nums[correct]){
-                    swap(nums, i, correct);
-                }else{
-                    i++;
-                }
+    public static int firstMissingPositive(int[] nums) {
 
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
             }
 
-            //
-            for(int index=0 ; index<nums.length; index++){
-                if(nums[index] != index+1){
-                    return index + 1;
-                }
+        }
+
+        //
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
             }
-
-            return nums.length+1;
         }
 
-        public static void swap(int [] nums, int first, int second){
+        return nums.length + 1;
+    }
 
-            int temp=nums[first];
-            nums[first]=nums[second];
-            nums[second]=temp;
-        }
+    public static void swap(int[] nums, int first, int second) {
 
-
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
 
 }
