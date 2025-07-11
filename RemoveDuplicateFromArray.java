2dfe484abcefccbc39eabba8d8db511492f22d7e
vedant Kale <vedantkale0707@gmail.com>
2025-07-11 08:18:43 +0530

    removeDuplicate from array program is added

--- RemoveDuplicateFromArray.java
+++ RemoveDuplicateFromArray.java
@@ -1,2 +1,18 @@
-package com.leedcodeQuestions;public class RemoveDuplicateFromArray {
+package com.leedcodeQuestions;
+
+public class RemoveDuplicateFromArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int removeDuplicate(int nums[]){
+        int i=0;
+        for(int n:nums){
+            if(i<2 || n!= nums[i-2]){
+                nums[i++]=n;
+            }
+        }
+        return i;
+    }
 }
