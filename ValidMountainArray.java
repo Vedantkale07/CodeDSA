c53d3d73a2eff565c64b1f633a8a9376a0a58e10
vedant Kale <vedantkale0707@gmail.com>
2025-07-20 19:06:52 +0530

    array program is added

---
+++ ValidMountainArray.java
@@ -0,0 +1,19 @@
+package com.leedcodeQuestions;
+
+public class ValidMountainArray {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public boolean validMountain(int arr[]){
+
+       int n= arr.length;
+       if(n<3) return false;
+
+       for(int i=1; i<n-1; i++){
+           
+       }
+    }
+}
